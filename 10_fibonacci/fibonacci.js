a19ca7505d8f6bc8185789350f332c074ae540ee
Priyanshu <priyanshujain6008@gmail.com>
2023-02-02 19:54:31 +0530

    first

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let n=0;
+    let m=1;
+    let c;
+    if(a<0)return "OOPS";
+    else if(a==1)return 1;
+    else if(a>0){
+        for(let i=0;i<a-1;i++){ 
+            c=n+m;
+            n=m;
+            m=c;
+        }
+        return c;
+    }
 };
 
 // Do not edit below this line
